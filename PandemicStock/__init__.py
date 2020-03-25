d1fda7d00852d8e7b7a58b617094703d851d4217
Atlchris67 <atlchris@gmail.com>
2020-03-24 20:29:47 -0400

    Fix app imports

--- PandemicStock/__init__.py
+++ PandemicStock/__init__.py
@@ -6,6 +6,6 @@ The flask application package.
 from flask import Flask
 app = Flask(__name__) 
 
-import FlaskWebProject.views
-import FlaskWebProject.apis
+import PandemicStock.views
+import PandemicStock.apis
 
